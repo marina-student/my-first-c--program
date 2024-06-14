6957b0a86a3cdf9b34ecdc5909eb1929932ef861
marina-student <pasxalismarina@gmail.com>
2024-06-14 22:41:59 +0300

    Create main.cs
    No comment!

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Treading.Tasks;
+
+namespace CSarpTutorials
+{
+     class Program
+     {
+         static void Main(string[] args)
+
+string message= "Hallo World!";
+
+Console.Writeline(message);
+}
+}
+}
